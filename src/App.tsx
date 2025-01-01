19e4fb19f9bb19e3edd9116d3d2a32a09cdcca1e
Kaleeswaran V <vkalees64@gmail.com>
2025-01-01 05:33:35 +0530

    feat: sweet alert update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,30 @@ import Swal from "sweetalert2";
 import "./App.css";
 
 function App() {
-  const openAlert = () => {
-    Swal.fire("Welcome to React", "", "success");
+  const openNormalAlert = () => {
+    alert("Hello, World!");
   };
+
+  const openSweetAlert = () => {
+    // alert("Hello, World!");
+    Swal.fire("Hello World", "", "success");
+  };
+
   return (
     <main>
-      <button onClick={openAlert}>Open Sweet Alert</button>
+      <h1 className="text-4xl font-bold text-center">React</h1>
+      <h1
+        className="text-2xl font-bold text-center cursor-pointer p-2 rounded border-black border hover:text-white hover:bg-black mx-auto w-40 my-4"
+        onClick={openNormalAlert}
+      >
+        ALert
+      </h1>
+      <h1
+        className="text-2xl font-bold text-center cursor-pointer p-2 rounded border-black border hover:text-white hover:bg-black mx-auto w-40 my-4"
+        onClick={openSweetAlert}
+      >
+        SweetALert
+      </h1>
     </main>
   );
 }
