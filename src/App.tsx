ac0a9b917caff29008e3a02bf5a5379333ddc8e9
Kaleeswaran V <vkalees64@gmail.com>
2025-01-06 04:46:31 +0530

    feat: sonner toast msg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Swal from "sweetalert2";
 import "./App.css";
+import { toast, Toaster } from "sonner";
 
 function App() {
   const openNormalAlert = () => {
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <main>
+      <Toaster richColors position="top-right" />
       <h1 className="text-4xl font-bold text-center">React</h1>
       <h1
         className="text-2xl font-bold text-center cursor-pointer p-2 rounded border-black border hover:text-white hover:bg-black mx-auto w-40 my-4"
@@ -25,6 +27,14 @@ function App() {
         onClick={openSweetAlert}
       >
         SweetALert
+      </h1>
+      <h1
+        className="text-2xl font-bold text-center cursor-pointer p-2 rounded border-black border hover:text-white hover:bg-black mx-auto w-40 my-4"
+        onClick={() => {
+          toast.error("We got it!");
+        }}
+      >
+        Toaster
       </h1>
     </main>
   );
